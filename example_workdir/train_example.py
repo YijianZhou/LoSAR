f90a5dc5fe48b26de0767930f5d235a7a9814c55
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2021-05-30 16:19:28 +0800

    Update train_example.py

--- example_workdir/train_example.py
+++ example_workdir/train_example.py
@@ -4,8 +4,8 @@ import os, shutil
 import warnings
 warnings.filterwarnings("ignore")
 
-cdrp_dir = '/home/zhouyj/software/CERP_Pytorch'
-shutil.copyfile('config_example.py', os.path.join(cdrp_dir, 'config.py'))
+cerp_dir = '/home/zhouyj/software/CERP_Pytorch'
+shutil.copyfile('config_example.py', os.path.join(cerp_dir, 'config.py'))
 # train params
 gpu_idx = 1
 model = ['DetNet','PpkNet'][0]
@@ -17,10 +17,10 @@ resume = [False, True][0]
 if model=='DetNet':
   os.system("python {}/train_cnn.py --gpu_idx={} \
     --zarr_path={} --ckpt_dir={} --resume={} "\
-    .format(cdrp_dir, gpu_idx, zarr_path, ckpt_dir, resume))
+    .format(cerp_dir, gpu_idx, zarr_path, ckpt_dir, resume))
 elif model=='PpkNet':
   os.system("python {}/train_rnn.py --gpu_idx={}\
     --zarr_path={} --ckpt_dir={} --resume={} "\
-    .format(cdrp_dir, gpu_idx, zarr_path, ckpt_dir, resume))
+    .format(cerp_dir, gpu_idx, zarr_path, ckpt_dir, resume))
 else: print('false model name!')
 
