7e80d82f8253513c63301d875b4ec3803a34a32f
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-07-21 14:04:25 -0700

    Update picker_stream.py

--- picker_stream.py
+++ picker_stream.py
@@ -70,7 +70,7 @@ class CERP_Picker_Stream(object):
     if len(stream)!=num_chn: return 
     net, sta = stream[0].stats.network, stream[0].stats.station
     net_sta = '%s.%s'%(net,sta)
-    start_time, end_time = stream[0].stats.starttime, stream[0].stats.endtime
+    start_time, end_time = stream[0].stats.starttime+win_stride, stream[0].stats.endtime
     if end_time < start_time + win_len: return 
     num_win = int((end_time - start_time - win_len) / win_stride) + 1
     st_len_npts = min([len(trace) for trace in stream])
