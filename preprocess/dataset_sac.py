5bd7f193cbf4bfdf654025f9ec82113378eb54dc
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2023-08-26 16:35:05 -0700

    Update dataset_sac.py

--- preprocess/dataset_sac.py
+++ preprocess/dataset_sac.py
@@ -12,24 +12,23 @@ cfg = config.Config()
 samp_rate = cfg.samp_rate
 num_chn = cfg.num_chn
 num_steps = cfg.num_steps
-win_len = int(cfg.win_len * cfg.samp_rate)
-step_len = int(cfg.step_len * cfg.samp_rate)
-step_stride = int(cfg.step_stride * cfg.samp_rate)
+win_len_npts = int(cfg.win_len * samp_rate)
+step_len_npts = int(cfg.step_len * samp_rate)
+step_stride_npts = int(cfg.step_stride * samp_rate)
 
 
 class Events(Dataset):
   """ Data pipeline for Event samples
   """
   def __init__(self, sample_list):
-    self.win_len = win_len
+    self.win_len = win_len_npts
     self.num_chn = cfg.num_chn
     self.samples = np.load(sample_list)
     np.random.shuffle(self.samples)
 
   def __getitem__(self, index):
-    # set label
-    st_paths = self.samples[index]
     # read data
+    st_paths = self.samples[index]
     st = Stream([read(st_path)[0] for st_path in st_paths])
     # output (data, target)
     data = np.zeros([self.num_chn, self.win_len], dtype=np.float32)
@@ -48,18 +47,17 @@ class Sequences(Dataset):
   def __init__(self, pos_list):
     self.num_chn = num_chn
     self.num_steps = num_steps
-    self.step_len = step_len
-    self.step_stride = step_stride
+    self.step_len = step_len_npts
+    self.step_stride = step_stride_npts
     self.samples = np.load(pos_list)
     self.get_seq_target = get_seq_target
 
   def __getitem__(self, index):
-    # set label
-    pos_paths = self.samples[index]
     # read data
-    st_pos = Stream([read(st_path)[0] for st_path in pos_paths])
+    st_paths = self.samples[index]
+    st = Stream([read(st_path)[0] for st_path in st_paths])
     # get target
-    header = st_pos[0].stats.sac
+    header = st[0].stats.sac
     tp, ts = header.t0, header.t1
     target_seq = self.get_seq_target(tp, ts)
     # output (data, target)
@@ -68,14 +66,13 @@ class Sequences(Dataset):
       for chn_idx in range(self.num_chn):
         chn_idx_0 = chn_idx * self.step_len
         step_idx_0 = step_idx * self.step_stride
-        if len(st_pos[chn_idx]) < step_idx_0+self.step_len: continue
+        if len(st[chn_idx]) < step_idx_0+self.step_len: continue
         data_seq[step_idx, chn_idx_0 : chn_idx_0 + self.step_len] = \
-            st_pos[chn_idx].data[step_idx_0 : step_idx_0 + self.step_len]
+            st[chn_idx].data[step_idx_0 : step_idx_0 + self.step_len]
     return data_seq, target_seq
 
   def __len__(self):
     return len(self.samples)
-
 
 # get target of sequence samples
 def get_seq_target(tp, ts):
@@ -89,4 +86,3 @@ def get_seq_target(tp, ts):
     target_p = np.ones(idx_s-idx_p, dtype=np.int_)
     target_s = 2*np.ones(num_steps-idx_s, dtype=np.int_)
     return np.concatenate([target_n, target_p, target_s])
-
