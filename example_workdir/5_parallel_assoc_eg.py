627888b234f08de4d9f3dcc5dec2a3f115d54538
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2023-08-26 17:43:30 -0700

    Update 5_parallel_assoc_eg.py

--- example_workdir/5_parallel_assoc_eg.py
+++ example_workdir/5_parallel_assoc_eg.py
@@ -1,14 +1,15 @@
 import os, shutil
 from obspy import UTCDateTime
 
-# parallel params
+# i/o paths
 pal_dir = '/home/zhouyj/software/PAL'
-shutil.copyfile('config_pal_eg.py', os.path.join(pal_dir, 'config.py'))
+shutil.copyfile('config_eg.py', os.path.join(pal_dir, 'config.py'))
+sta_file = 'input/example_pal_format1.sta'
+pick_dir = 'output/eg/picks'
+out_root = 'output/eg'
+# parallel params
 time_range = '20190704-20190707'
 num_workers = 3
-out_root = 'output/eg'
-pick_dir = 'output/eg'
-sta_file = 'input/example_pal_format1.sta'
 
 # divide by time
 start_date, end_date = [UTCDateTime(date) for date in time_range.split('-')]
@@ -17,8 +18,8 @@ for proc_idx in range(num_workers):
     t0 = ''.join(str((start_date + proc_idx*dt).date).split('-'))
     t1 = ''.join(str((start_date + (proc_idx+1)*dt).date).split('-'))
     time_range = '{}-{}'.format(t0, t1)
-    out_pha = '{}/phase_{}.dat'.format(out_root, t0)
-    out_ctlg = '{}/catalog_{}.dat'.format(out_root, t0)
+    out_pha = '{}/phase_{}.dat'.format(out_root, time_range)
+    out_ctlg = '{}/catalog_{}.dat'.format(out_root, time_range)
     os.system("python {}/run_assoc.py \
         --time_range={} --pick_dir={} --sta_file={} \
         --out_ctlg={} --out_pha={} & " \
